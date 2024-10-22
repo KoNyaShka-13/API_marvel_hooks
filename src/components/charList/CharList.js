4c00b298176dd9b0426ab0293ab5867f0ef3028f
KoNyaShKa <ggiziawpgg@gmail.com>
2024-10-22 08:55:15 +0300

    upgrade using hooks

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 
 import Spinner from '../Spinner/Spinner';
@@ -6,73 +6,92 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelServices';
 import './charList.scss';
 
-class CharList extends Component {
+const CharList = (props) => {
 
-    state = {
-        charList: [],
-        loading: true,//Первичная загрузка 
-        error: false,
-        newItemLoading: false,//Загрузка персонажей дополнительных
-        offset: 210,//Не путать с отсупом в MarvelServices
-        charEnded: false,//Значение, когда закончились персонажи
-    }
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(210);
+    const [charEnded, setCharEnded] = useState(false);
+
+//    state = {//Каждый state будем оборачивать в useState
+//        charList: [],
+//        loading: true,//Первичная загрузка 
+//        error: false,
+//        newItemLoading: false,//Загрузка персонажей дополнительных
+//        offset: 210,//Не путать с отсупом в MarvelServices
+//        charEnded: false,//Значение, когда закончились персонажи
+//    }
     
-    marvelService = new MarvelService();
+    const marvelService = new MarvelService();
 
-    componentDidMount() {//Запрос, когда компонент был создан на странице
-        this.onRequest();//Код один и тот же, по этому данный компонент вызывает onReaquest, но без отступа offset, аргумент будет пустой и использоваться, что задан раньше в MarvelServices
+    useEffect(() => {//useEffect запускается после рендера, по этому нестрашно, если он находится выше запроса, что в нем находится
+        onRequest();
+    }, [])
+
+    //componentDidMount() {//Запрос, когда компонент был создан на странице
+    //   this.onRequest();//Код один и тот же, по этому данный компонент вызывает onReaquest, но без отступа offset, аргумент будет пустой и использоваться, что задан раньше в MarvelServices
+    //}
+
+    const onRequest = (offset) => {//Запрос с нужными отступами, которые можно изменять по мере необходимости
+        onCharListLoading();
+        marvelService.getAllCharacters(offset)
+            .then(onCharListLoaded)
+            .catch(onError)
     }
 
-    onRequest = (offset) => {//Запрос с нужными отступами, которые можно изменять по мере необходимости
-        this.onCharListLoading();
-        this.marvelService.getAllCharacters(offset)
-            .then(this.onCharListLoaded)
-            .catch(this.onError)
+    const onCharListLoading = () => {
+        setNewItemLoading(true);
     }
 
-    onCharListLoading = () => {//Начался процесс загрузки
-        this.setState({
-            newItemLoading: true
-        })
-    }
+    //onCharListLoading = () => {//Начался процесс загрузки
+    //    this.setState({
+    //        newItemLoading: true
+    //    })
+    //}
  
-    onCharListLoaded = (newcharList) => {
+    const onCharListLoaded = (newCharList) => {
         let ended = false;
-        if (newcharList.length < 9) {//Проверка, если в последний раз загрузилось меньше 9 персонажей, значит в массиве данных больше не осталось их, а значит, ставим пометку, что они закончились, чтобы сделать кнопу не активной
+        if (newCharList.length < 9) {//Проверка, если в последний раз загрузилось меньше 9 персонажей, значит в массиве данных больше не осталось их, а значит, ставим пометку, что они закончились, чтобы сделать кнопу не активной
             ended = true;
         }
 
-        this.setState(({offset, charList}) => ({
-            charList: [...charList, ...newcharList],
-            loading: false,
-            newItemLoading: false,//Когда персонажи загрузились, загрузка завершается
-            offset: offset + 96,
-            charEnded: ended
-        }))
+        //this.setState(({offset, charList}) => ({
+        //    charList: [...charList, ...newcharList],
+        //    loading: false,
+        //    newItemLoading: false,//Когда персонажи загрузились, загрузка завершается
+        //    offset: offset + 96,
+        //    charEnded: ended
+        //}))
+        setCharList(charList => [...charList, ...newCharList]);//Каждое состояние в данный момент - это отдельная переменная
+        setLoading(loading => false);//Можно через коллбэк функцию, как указано  тут, а можно просто фолс оставить, так как неважно, какое значение было раньше, до обновления в данном случае
+        setNewItemLoading(newItemLoading => false);
+        setOffset(offset => offset + 9);
+        setCharEnded(charEnded => ended);
     }
 
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
+    const onError = () => {
+        setError(true);//Можно писать и так, как тут, и так, как ниже указано
+        setLoading(loading => false);
     }
 
     //Пример использования ref
-    itemRefs = [];
+    //itemRefs = [];
+    const itemRefs = useRef([]);//Рефы без каррент нельзя использовать, в отличие от обычных рефов
 
-    setRef = (ref) => {
-        this.itemRefs.push(ref);
-    }
+    //setRef = (ref) => {
+    //    this.itemRefs.push(ref);
+    //}
 
-    focusOnItem = (id) => {
+    const focusOnItem = (id) => {
         //Но рефами можно пользоваться только в конкретных случаях
-        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-        this.itemRefs[id].classList.add('char__item_selected');
-        this.itemRefs[id].focus();
+        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+        itemRefs.current[id].classList.add('char__item_selected');
+        itemRefs.current[id].focus();
     }
     // Этот метод создан для оптимизации, чтобы не помещать такую конструкцию в метод render
-    renderItems(arr) {
+    function renderItems(arr) {
         const items =  arr.map((item,i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
@@ -83,16 +102,16 @@ class CharList extends Component {
                 <li 
                     className="char__item"
                     tabIndex={0}
-                    ref={this.setRef}
+                    ref={el => itemRefs.current[i] = el}//Делаем обычный перебор, так как пуш может дать ошибку, а здесь есть возможность использовать его
                     key={item.id}
                     onClick={() => {
-                        this.props.onCharSelected(item.id);
-                        this.focusOnItem(i);
+                        props.onCharSelected(item.id);
+                        focusOnItem(i);
                     }}
                     onKeyPress={(e) => {
                         if (e.key === ' ' || e.key === "Enter") {
-                            this.props.onCharSelected(item.id);
-                            this.focusOnItem(i);
+                            props.onCharSelected(item.id);
+                            focusOnItem(i);
                         }
                     }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
@@ -108,11 +127,9 @@ class CharList extends Component {
         )
     }
 
-    render() {
-
-        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+    
         
-        const items = this.renderItems(charList);
+        const items = renderItems(charList);
 
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
@@ -127,13 +144,12 @@ class CharList extends Component {
                     className="button button__main button__long"
                     disabled={newItemLoading}
                     style={{'display': charEnded ? 'none' : 'block'}}
-                    onClick={() => this.onRequest(offset)}>
+                    onClick={() => onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
         )
     }
-}
 
 CharList.propTypes = {//Проверка пропса и функции, которая относится к нему
     onCharSelected: PropTypes.func.isRequired
